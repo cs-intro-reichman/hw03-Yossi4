7e30e6074639a4692abc2c9ca039c356b9a25949
Yossi4 <154239866+Yossi4@users.noreply.github.com>
2024-01-07 19:39:50 +0200

    added file

---
+++ Calendar.java
@@ -0,0 +1,8 @@
+public class Calendar {	
+	
+	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
+	public static void main(String args[]) {
+		int year = Integer.parseInt(args[0]);
+		
+	}
+}
